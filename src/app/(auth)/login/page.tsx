ea15152c7754910bbf56035939114a5a1d2f7c19
Jaleed khan <jaleedkhan53@gmail.com>
2026-04-18 11:40:29 +0500

    for understanding route. i created the files

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoginPage = () => {
+    return (
+        <div>
+            <h3> this is the login page </h3>
+        </div>
+    )
+};
+
+export default LoginPage;
---
+++ src/app/contact/edit/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+const EditContactPage = () => {
+    return (
+        <div>
+            <h3>This is the Edit PAGE</h3>
+        </div>
+    )
+
+};
+
+export default EditContactPage;
