20659f1f5bff06b48b471a061192969a9d8d8cc4
Jaleed khan <jaleedkhan53@gmail.com>
2026-04-23 14:52:50 +0500

    logout button done.

--- src/app/_component/LogOutButton.tsx
+++ src/app/_component/LogOutButton.tsx
@@ -1,10 +1,20 @@
+"use client"
 import React from "react";
+import { logoutAction } from "../actions/auth";
+import { useRouter } from "next/navigation";
 const LogOutButton = () => {
-    return (
-        <div>
-            Logout Button
-        </div>
-    )
+    const router = useRouter();
+    const handlerLogout = async () => {
+        try{
+            await logoutAction();
+            router.push("/login");
+            router.refresh();
+
+        }catch(error){
+            console.log("Logout Error:",error);
+        }
+    }
+    return <button className="text-white bg-red-500 hover:bg-red-700 px-4 py-2 rounded-md cursor-pointer" onClick={handlerLogout}>Logout</button>
 };
 
 export default LogOutButton;
